95388f025176a24080c3b14d2ef82af3a03d0fb6
오세진 <cadinz9096@gmail.com>
2019-03-14 22:48:29 +0900

    Add Kakao map SDk

--- app/src/main/java/com/yanawha/osejin/yanawha/MainActivity.java
+++ app/src/main/java/com/yanawha/osejin/yanawha/MainActivity.java
@@ -1,13 +1,28 @@
 package com.yanawha.osejin.yanawha;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
     }
-}
+
+
+
+
+    }
+
