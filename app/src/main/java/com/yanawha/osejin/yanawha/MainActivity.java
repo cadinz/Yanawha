a7829ec2ffb4bf4cf60dafacbc4369c510c76718
오세진 <cadinz9096@gmail.com>
2019-03-14 23:08:11 +0900

    Add Google Places api

--- app/src/main/java/com/yanawha/osejin/yanawha/MainActivity.java
+++ app/src/main/java/com/yanawha/osejin/yanawha/MainActivity.java
@@ -1,10 +1,23 @@
 package com.yanawha.osejin.yanawha;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+
 import net.daum.mf.map.api.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,17 +25,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-
         MapView mapView = new MapView(this);
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
+        findViewById(R.id.fab_search).setOnClickListener(new View.OnClickListener( ) {
+            @Override
+            public void onClick(View v) {
+                try {
+                    AutocompleteFilter autocompleteFilter = new AutocompleteFilter.Builder()
+                            .setTypeFilter(Place.TYPE_COUNTRY)
+                            .setCountry("KOR")
+                            .build();
+                    Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                            .setFilter(autocompleteFilter)
+                            .build(MainActivity.this);
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    // TODO: Handle the error.
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                }
+            }
+        });
     }
 
-
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String toastMsg = String.format("Place: %s", data.toString( ));
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show( );
+            }
+        }
     }
+
+}
+
+
+
+
 
