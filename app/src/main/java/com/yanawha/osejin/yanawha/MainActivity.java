fc602b5ba41c9c8ee670658a9ab3830265f8648e
오세진 <cadinz9096@gmail.com>
2019-03-17 02:30:34 +0900

    Add custom markers

--- app/src/main/java/com/yanawha/osejin/yanawha/MainActivity.java
+++ app/src/main/java/com/yanawha/osejin/yanawha/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -14,15 +16,104 @@ import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 
+import net.daum.mf.map.api.CalloutBalloonAdapter;
+import net.daum.mf.map.api.CameraUpdateFactory;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPointBounds;
 import net.daum.mf.map.api.MapView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MapView.POIItemEventListener, MapView.MapViewEventListener{
+
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    class CustomCalloutBalloonAdapter implements CalloutBalloonAdapter {
+        private final View mCalloutBalloon;
+
+        public CustomCalloutBalloonAdapter() {
+            mCalloutBalloon = getLayoutInflater().inflate(R.layout.custom_callout_balloon, null);
+        }
+
+        @Override
+        public View getCalloutBalloon(MapPOIItem poiItem) {
+            ((TextView) mCalloutBalloon.findViewById(R.id.text)).setText(poiItem.getItemName());
+            return mCalloutBalloon;
+        }
+
+        @Override
+        public View getPressedCalloutBalloon(MapPOIItem poiItem) {
+            return null;
+        }
+    }
+
+
 
     int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
     private MapPOIItem marker;
@@ -30,19 +121,23 @@ public class MainActivity extends AppCompatActivity {
     private MapView mapView;
     private ViewGroup mapViewContainer;
     private String selectedPlace;
-    private ArrayList<MapPOIItem> markers;
+    private ArrayList<MapPOIItem> markers = new ArrayList<MapPOIItem>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //Map view init
         mapView = new MapView(this);
+        mapView.setCalloutBalloonAdapter(new CustomCalloutBalloonAdapter());
         mapViewContainer = findViewById(R.id.map_view);
+        mapView.setMapViewEventListener(this);
+        mapView.setPOIItemEventListener(this);
         mapViewContainer.addView(mapView);
 
 
 
+
+        Log.d("tag", "onCreate: start go");
 
 
 
@@ -66,40 +161,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    void addMarker(Place place){
+        final LatLng latLng = place.getLatLng();
+        marker = new MapPOIItem();
+        marker.setItemName(selectedPlace);
+        marker.setTag(0);
+        mapPoint = MapPoint.mapPointWithGeoCoord(latLng.latitude, latLng.longitude);
+        marker.setMapPoint(mapPoint);
+        // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        //ArrayList marker 추가
+        markers.add(marker);
+
+        mapView.addPOIItem(marker);
+        mapView.selectPOIItem(marker, true);
+        mapView.setMapCenterPoint(marker.getMapPoint(), false);
+
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                Place place = PlaceAutocomplete.getPlace(this, data);
+                final Place place = PlaceAutocomplete.getPlace(this, data);
 
                 if (place != null) {
-
-                    final LatLng latLng = place.getLatLng();
-                    String SN = "Lat=" + String.valueOf(latLng.latitude) +
-                            "Lng=" + String.valueOf(latLng.longitude);
-                    Log.d("SN.toString()", SN);
 
                     //dialog event
                     CustomDialog alert = new CustomDialog(MainActivity.this);
                     selectedPlace = place.getName( ).toString( );
                     alert.setTvSelectedPlace(selectedPlace);
                     alert.setDialogListener(new MyDialogListener( ) {
+
                         @Override
                         public void onPositiveClicked() {
-                            marker = new MapPOIItem();
-                            marker.setItemName(selectedPlace);
-                            marker.setTag(0);
-                            mapPoint = MapPoint.mapPointWithGeoCoord(latLng.latitude, latLng.longitude);
-                            marker.setMapPoint(mapPoint);
-                            // 기본으로 제공하는 BluePin 마커 모양.
-                            marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
-                            // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-                            marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
-                            mapView.addPOIItem(marker);
-                            //ArrayList marker 추가
-                            markers.add(marker);
-
-
+                            //검색한 장소의 마커를 추가합니다.
+                            addMarker(place);
                             Toast.makeText(MainActivity.this, "확인", Toast.LENGTH_SHORT).show( );
                         }
 
@@ -118,6 +219,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
 
 
 
